5447dde89291254cbdd181574a854ebf284219c9
Gittrym12 <trainingdev99@gmail.com>
2024-10-10 16:16:51 +0700

    change urls request training to more readible

--- apps/forms/permintaan_training/urls.py
+++ apps/forms/permintaan_training/urls.py
@@ -15,15 +15,15 @@ from .views import (
 app_name = 'permintaan_training'
 
 urlpatterns = [
-    path('request_training/', request_training_list, name='request_training_list'), # This is the URL for the public list view
-    path('dashboard/request_training/', admin_request_training_list, name='admin_request_training_list'), # This is the URL for the admin list view
-    path('request_training_user/', request_training_user, name='request_training_user'), # This is the URL for create training form
-    path('dashboard/create_training/', create_training, name='create_training'),
-    path('fetch_user_details/', fetch_user_details, name='fetch_user_details'), # This is the URL for fetching user details
-    path('dashboard/list_form_request_training_manager', manager_training_list, name='manager_request_training_list'), # This is the URL for the manager list view and approval
-    path('dashboard/list_form_request_training_gm', gm_training_list, name='gm_request_training_list'), # This is the URL for the GM list view and approval
-    path('dashboard/list_form_request_training_hrd', hrd_training_list, name='hrd_request_training_list'),  # This is the URL for the HRD Manager list view and approval
-    path('dashboard/request_training/<int:training_id>/', admin_request_training_view, name='admin_request_training_view'), # This is the URL for the admin view
+    path('list', request_training_list, name='request_training_list'), # This is the URL for the public list view
+    path('admin/lists', admin_request_training_list, name='admin_request_training_list'), # This is the URL for the admin list view
+    path('create', request_training_user, name='request_training_user'), # This is the URL for create training form
+    path('admin/create', create_training, name='create_training'),
+    path('fetch_user_details', fetch_user_details, name='fetch_user_details'), # This is the URL for fetching user details
+    path('manager/approval/list', manager_training_list, name='manager_request_training_list'), # This is the URL for the manager list view and approval
+    path('gm/approval/list', gm_training_list, name='gm_request_training_list'), # This is the URL for the GM list view and approval
+    path('manager_hrd/approval/list', hrd_training_list, name='hrd_request_training_list'),  # This is the URL for the HRD Manager list view and approval
+    path('admin/request_training/<int:training_id>/', admin_request_training_view, name='admin_request_training_view'), # This is the URL for the admin view
     path('trainings/<int:training_id>/delete/', admin_delete_training, name='admin_delete_training'), # This is the URL for the admin delete view
 
 ]
